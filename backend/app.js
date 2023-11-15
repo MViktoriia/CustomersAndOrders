ff25e1a59fd0e8a73ee425c46cf934425b12f224
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2023-11-15 17:21:55 +0200

    handle client-side routing

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,16 @@ app.use(express.json());
 app.use('/api/customers', customersRouter);
 app.use('/api/orders', ordersRouter);
 
+//Midleware serving static files and handle client-side routing
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
+const indexPath = path.join(__dirname, '..', 'frontend', 'dist', 'index.html');
+app.get('/customers', (req, res) => res.sendFile(indexPath));
+app.get('/orders', (req, res) => res.sendFile(indexPath));
+
+app.get('*', (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+});
+
 //Middlewares error route
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
